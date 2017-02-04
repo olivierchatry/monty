bd462231a06a4b53f97d88d4743cc43f78139721
Olivier Chatry <olivier@fieldap.com>
2017-02-04 17:36:14 +0100

    fix pstr going one char to far in some case

--- src/monty_instr_pstr.c
+++ src/monty_instr_pstr.c
@@ -2,10 +2,13 @@
 #include "monty.h"
 
 void monty_instr_pstr(monty_t *monty) {
-	int val = 1;
-	while (monty->dlist.count != 0 && val > 0 && val <= 127) {
-		val = monty_pop(monty);
-		printf("%c", val);
+	int valid = 1;
+	while (monty->dlist.count != 0 && valid) {
+		int val = monty_pop(monty);
+		valid = val > 0 && val <= 127; 
+		if (valid) {
+			printf("%c", val);
+		}
 	}
 	printf("\n");
 }
