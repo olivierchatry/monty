b00a493baccb0e873a9adf9517866e9caa4aefd0
Olivier Chatry <olivier@fieldap.com>
2017-02-05 12:58:40 +0100

    forgot to comit a file

--- src/instr/monty_instr_rotl.c
+++ src/instr/monty_instr_rotl.c
@@ -1,9 +1,9 @@
-#include "monty.h"
+#include "../monty.h"
 
 void monty_instr_rotl(monty_t *monty) {
-	if (monty->dlist.count > 0) {
-		monty_value_t v; 
-		v.ptr = dlist_pop_head(&monty->dlist);
-		dlist_push_tail(&monty->dlist, v.ptr);
+	if (monty->dl->count > 0) {
+		dlist_value_t v; 
+		v = dlist_pop_head(monty->dl);
+		dlist_push_tail(monty->dl, v);
 	}
 }
